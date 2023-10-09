7dde2d892ffe8aa542a897f532a3cddc3c43fbce
MironLitovets <litovets.mv@gmail.com>
2023-10-09 16:41:10 +1000

    Added Client project

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var message = "some message";
+
+            var data = Encoding.UTF8.GetBytes(message);
+
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(data);
+
+            var buff = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
+
+            do
+            {
+                size = tcpSocket.Receive(buff);
+                answer.Append(Encoding.UTF8.GetString(buff, 0, size));
+            }
+            while (tcpSocket.Available > 0);
+
+            Console.WriteLine(answer.ToString());
+
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+
+            Console.ReadLine(); // бесконечный цикл
+        }
+    }
+}
