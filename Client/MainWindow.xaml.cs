c0ff7036a547db6abfd485ada879d4b5cf931a75
MironLitovets <litovets.mv@gmail.com>
2023-10-10 18:17:14 +1000

    Deleted module which receive answer from server

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Client
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -29,15 +14,14 @@ namespace Client
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            IPAddress localIp = IPAddress.Loopback;
-            const int port = 8080;
+            const string ip = "192.168.94.224";
+            const int port = 5001;
 
-            var tcpEndPoint = new IPEndPoint(localIp, port);
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
 
             var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             var message = "change";
-
             var data = Encoding.UTF8.GetBytes(message);
 
             tcpSocket.Connect(tcpEndPoint);
