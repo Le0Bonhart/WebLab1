edda55c3268ac5ab330a3a0c87865945d80e0354
MironLitovets <litovets.mv@gmail.com>
2023-10-11 14:25:32 +1000

    Added exceptions of the wrong or incorrect IP address

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -15,20 +15,40 @@ namespace Client
         private void button_Click(object sender, RoutedEventArgs e)
         {
             string ip = textBox.Text;
+            IPAddress? ip_addr = null;
+
+            if (!IPAddress.TryParse(ip, out ip_addr))
+            {
+                label.Content = "Адрес не корректен";
+                return;
+            }
+
+            if (ip_addr.AddressFamily != AddressFamily.InterNetwork)
+            {
+                label.Content = "Неверный протокол";
+                return;
+            }
+
             const int port = 5001;
 
-            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            var tcpEndPoint = new IPEndPoint(ip_addr, port);
 
             var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             var message = "change";
             var data = Encoding.UTF8.GetBytes(message);
 
-            tcpSocket.Connect(tcpEndPoint);
-            tcpSocket.Send(data);
-
-            tcpSocket.Shutdown(SocketShutdown.Both);
-            tcpSocket.Close();
+            try
+            {
+                tcpSocket.Connect(tcpEndPoint);
+                tcpSocket.Send(data);
+                tcpSocket.Shutdown(SocketShutdown.Both);
+                tcpSocket.Close();
+            }
+            catch
+            {
+                label.Content = "Невозможно подключиться";
+            }
         }
     }
 }
